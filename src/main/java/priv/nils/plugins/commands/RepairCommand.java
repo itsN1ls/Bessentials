75eecf26d75b89b135513e619281f675ff51eda8
NilsoMC <nils.wirsing@outlook.de>
2023-06-26 18:35:12 +0200

    things

--- src/main/java/priv/nils/plugins/commands/RepairCommand.java
+++ src/main/java/priv/nils/plugins/commands/RepairCommand.java
@@ -1,6 +1,8 @@
 package priv.nils.plugins.commands;
 
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,7 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
-import org.bukkit.entity.Item;
 
 import static priv.nils.plugins.Bessentials.Prefix;
 
@@ -16,30 +17,51 @@ public class RepairCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(Prefix + NamedTextColor.RED + "error: player only");
+            sender.sendMessage(Prefix + NamedTextColor.RED + "Error: Only players can use this command.");
             return true;
         }
         Player player = (Player) sender;
 
-        if (!player.hasPermission("bessentials.repair") || !player.isOp()) {
+        if (!player.hasPermission("bessentials.repair") && !player.isOp()) {
             player.sendMessage("Unknown command. Type \"/help\" for help.");
             return true;
         }
-        if (args.length == 0) {
-            player.getItemInHand().setDurability((short) 0);
-            player.sendMessage(Prefix + "item "+player.getItemInHand().getItemMeta().displayName() + " repaired.");
+
+        if (args.length == 0 || (args.length == 1 && args[0].equalsIgnoreCase("all"))) {
+            ItemStack itemInHand = player.getItemInHand();
+
+            if (itemInHand == null || itemInHand.getType() == Material.AIR) {
+                player.sendMessage(Prefix + "You are not holding an item.");
+                return true;
+            }
+
+            if (args.length == 1 && args[0].equalsIgnoreCase("all")) {
+                for (ItemStack item : player.getInventory().getContents()) {
+                    repairItem(item);
+                }
+                player.sendMessage(Prefix + "All items repaired.");
+            } else {
+                repairItem(itemInHand);
+                player.sendMessage(Prefix + "Item " + getItemDisplayName(itemInHand) + " repaired.");
+            }
+        } else {
+            player.sendMessage(Prefix + "usage: /repair [all]");
         }
 
+        return true;
+    }
 
+    private void repairItem(ItemStack item) {
+        if (item != null && item.getType() != Material.AIR) {
+            item.setDurability((short) 0);
+        }
+    }
 
-
-
-
-
-
-
-
-
-        return false;
+    private String getItemDisplayName(ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        if (meta != null && meta.hasDisplayName()) {
+            return meta.getDisplayName();
+        }
+        return item.getType().name();
     }
 }
